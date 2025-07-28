79f767b19286fbc06b06f4a85ea69c87863988b9
akhlaque <akhlaque.ak@gmail.com>
2025-07-28 18:45:10 -0400

    comit

--- pi.c
+++ pi.c
@@ -7,28 +7,34 @@ int main()
     typedef ui unsigned int;
     double pi;
     double start_time, run_time;
+    ui n_threads = 32;
+
+    double *sum = new double[n_threads];
 
     step = 1.0 / (double)num_steps;
     double sum_all = 0;
 
     start_time = omp_get_wtime();
+    for(ui i=0; i<n_threads;i++)
+        sum[i] = 0;
 
 #pragma omp parallel
     {
+        n_threads = omp_get_num_threads();
         double x, sum = 0;
-        ui total_threads=omp_get_num_threads();
-        ui chunk = num_steps/total_threads+1;
+        ui chunk = num_steps/n_threads+1;
 
         ui id = omp_get_thread_num();
 
         for (int i = id*chunk+1; i <= num_steps && i<=(id+1)*chunk; i++)
         {
             x = (i - 0.5) * step;
-            sum = sum + 4.0 / (1.0 + x * x);
+            sum[id] = sum[id] + 4.0 / (1.0 + x * x);
         }
-        #pragma omp critical
-        sum_all += sum;
     }
+    for(ui i=0; i<n_threads;i++)
+        sum_all+=sum[i];
+
     pi = step * sum_all;
     run_time = omp_get_wtime() - start_time;
     printf("\n pi with %ld steps is %lf in %lf seconds\n ", num_steps, pi, run_time);
