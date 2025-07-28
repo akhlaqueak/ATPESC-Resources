4368354b0bf8fd92a03cf5b62b39b65951833271
akhlaque <akhlaque.ak@gmail.com>
2025-07-28 18:40:08 -0400

    comit

--- pi.c
+++ pi.c
@@ -2,24 +2,34 @@
 #include <omp.h>
 static long num_steps = 100000000;
 double step;
-int main ()
+int main()
 {
-	  int i;
-	  double x, pi, sum = 0.0;
-	  double start_time, run_time;
+    typedef ui unsigned int;
+    double pi;
+    double start_time, run_time;
 
-	  step = 1.0/(double) num_steps;
+    step = 1.0 / (double)num_steps;
+    double sum_all = 0;
 
-        	 
-	  start_time = omp_get_wtime();
+    start_time = omp_get_wtime();
 
-	  for (i=1;i<= num_steps; i++){
-		  x = (i-0.5)*step;
-		  sum = sum + 4.0/(1.0+x*x);
-	  }
+#pragma omp parallel
+    {
+        double x, sum = 0;
+        ui total_threads=omp_get_num_threads();
+        ui chunk = num_steps/total_threads+1;
 
-	  pi = step * sum;
-	  run_time = omp_get_wtime() - start_time;
-	  printf("\n pi with %ld steps is %lf in %lf seconds\n ",num_steps,pi,run_time);
-}	  
+        ui id = omp_get_thread_num();
 
+        for (int i = id*chunk+1; i <= num_steps && i<=(id+1)*chunk; i++)
+        {
+            x = (i - 0.5) * step;
+            sum = sum + 4.0 / (1.0 + x * x);
+        }
+        #pragma omp critical
+        sum_all += sum;
+    }
+    pi = step * sum_all;
+    run_time = omp_get_wtime() - start_time;
+    printf("\n pi with %ld steps is %lf in %lf seconds\n ", num_steps, pi, run_time);
+}
