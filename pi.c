3948aa82bbe3abfa6502c8678225d51e6f42d43e
akhlaque <akhlaque.ak@gmail.com>
2025-07-29 10:01:10 -0400

    comit

--- pi.c
+++ pi.c
@@ -6,33 +6,30 @@ int main()
 {
     double pi;
     double start_time, run_time;
-    unsigned int n_threads = 32;
-
-    double *sum = new double[n_threads];
 
     step = 1.0 / (double)num_steps;
     double sum_all = 0;
 
     start_time = omp_get_wtime();
-    for(unsigned int i=0; i<n_threads;i++)
-        sum[i] = 0;
 
 #pragma omp parallel
     {
-        n_threads = omp_get_num_threads();
+        double sum = 0;
+        unsigned int n_threads = omp_get_num_threads();
         double x, sum = 0;
-        unsigned int chunk = num_steps/n_threads+1;
+        unsigned int chunk = num_steps / n_threads + 1;
 
         unsigned int id = omp_get_thread_num();
 
-        for (int i = id*chunk+1; i <= num_steps && i<=(id+1)*chunk; i++)
+        for (int i = id; i < num_steps; i += n_threads)
         {
-            x = (i - 0.5) * step;
-            sum[id] += 4.0 / (1.0 + x * x);
+            x = (i + 0.5) * step;
+            sum += 4.0 / (1.0 + x * x);
         }
+
+#pragma omp critical
+        sum_all += sum;
     }
-    for(unsigned int i=0; i<n_threads;i++)
-        sum_all+=sum[i];
 
     pi = step * sum_all;
     run_time = omp_get_wtime() - start_time;
