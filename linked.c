efaa5f55f0a54e87c705bf8691fb5c1d7d52421d
akhlaque <akhlaque.ak@gmail.com>
2025-07-29 13:25:41 -0400

    comit

--- linked.c
+++ linked.c
@@ -105,7 +105,7 @@ int main(int argc, char *argv[])
         size++;
     }
 
-    node* arr=new node[size];
+    node** arr=new node[size];
     unsigned int i=0;
     while(p!=NULL){
         arr[i++] = p;
@@ -115,7 +115,7 @@ int main(int argc, char *argv[])
     for(unsigned int i=0;i<size;i++){
         processwork(arr[i]);
     }
-    
+
     end = omp_get_wtime();
 
     // traverse the list releasing memory allocated for the list
