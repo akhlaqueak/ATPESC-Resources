516487fef5ae58a0b2c25f8c5de49d6640b325bf
akhlaque <akhlaque.ak@gmail.com>
2025-07-29 12:10:40 -0400

    comit

--- mandel.c
+++ mandel.c
@@ -6,8 +6,8 @@
 **           WARNING: this program may contain errors
 **
 **  USAGE:   Program runs without input ... just run the executable
-**            
-**  HISTORY: 
+**
+**  HISTORY:
 **.          Written:  (Mark Bull, August 2011).
 **           Simplified so you can do the exercise with less C experience (Tim Mattson, Nov 2023).
 */
@@ -17,58 +17,63 @@
 #include <math.h>
 #include <omp.h>
 
-# define NPOINTS 1000
-# define MAXITER 1000
+#define NPOINTS 1000
+#define MAXITER 1000
 
 void testpoint(double, double);
 
 int numoutside = 0;
 
-int main(){
-   int i, j;
-   double area, error, eps  = 1.0e-5;
-   double cimag, creal;
+int main()
+{
+    int i, j;
+    double area, error, eps = 1.0e-5;
+    double cimag, creal;
 
-
-//   Loop over grid of points in the complex plane which contains the Mandelbrot set,
-//   testing each point to see whether it is inside or outside the set.
+    //   Loop over grid of points in the complex plane which contains the Mandelbrot set,
+    //   testing each point to see whether it is inside or outside the set.
 
 #pragma omp parallel for private(eps, creal, cimag)
-   for (i=0; i<NPOINTS; i++) {
-     for (j=0; j<NPOINTS; j++) {
-       creal = -2.0+2.5*(double)(i)/(double)(NPOINTS)+eps;
-       cimag = 1.125*(double)(j)/(double)(NPOINTS)+eps;
-       testpoint(creal, cimag);
-     }
-   }
+    for (i = 0; i < NPOINTS; i++)
+    {
+        for (j = 0; j < NPOINTS; j++)
+        {
+            creal = -2.0 + 2.5 * (double)(i) / (double)(NPOINTS) + eps;
+            cimag = 1.125 * (double)(j) / (double)(NPOINTS) + eps;
+            testpoint(creal, cimag);
+        }
+    }
 
-// Calculate area of set and error estimate and output the results
-   
-area=2.0*2.5*1.125*(double)(NPOINTS*NPOINTS-numoutside)/(double)(NPOINTS*NPOINTS);
-   error=area/(double)NPOINTS;
+    // Calculate area of set and error estimate and output the results
 
-   printf("Area of Mandlebrot set = %12.8f +/- %12.8f\n",area,error);
-   printf("Correct answer should be around 1.510659\n");
+    area = 2.0 * 2.5 * 1.125 * (double)(NPOINTS * NPOINTS - numoutside) / (double)(NPOINTS * NPOINTS);
+    error = area / (double)NPOINTS;
 
+    printf("Area of Mandlebrot set = %12.8f +/- %12.8f\n", area, error);
+    printf("Correct answer should be around 1.510659\n");
 }
 
-void testpoint(double creal, double cimag){
+void testpoint(double creal, double cimag)
+{
 
-// iterate z=z*z+c, until |z| > 2 when point is known to be outside set
-// If loop count reaches MAXITER, point is considered to be inside the set
+    // iterate z=z*z+c, until |z| > 2 when point is known to be outside set
+    // If loop count reaches MAXITER, point is considered to be inside the set
 
-       double zreal, zimag, temp;
-       int iter;
-       zreal = creal;  zimag = cimag;
+    double zreal, zimag, temp;
+    int iter;
+    zreal = creal;
+    zimag = cimag;
 
-       for (iter=0; iter<MAXITER; iter++){
-         temp = (zreal*zreal)-(zimag*zimag)+creal;
-         zimag = zreal*zimag*2+cimag;
-         zreal = temp;
-         if ((zreal*zreal+zimag*zimag)>4.0) {
-           numoutside++;
-           break;
-         }
-       }
+    for (iter = 0; iter < MAXITER; iter++)
+    {
+        temp = (zreal * zreal) - (zimag * zimag) + creal;
+        zimag = zreal * zimag * 2 + cimag;
+        zreal = temp;
+        if ((zreal * zreal + zimag * zimag) > 4.0)
+        {
+            #pragma omp critical
+            numoutside++;
+            break;
+        }
+    }
 }
-
