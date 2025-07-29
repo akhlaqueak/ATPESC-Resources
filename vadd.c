83ea82d2b7c51c881a572dae3ce7cc1aaa54363a
akhlaque <akhlaque.ak@gmail.com>
2025-07-29 18:24:51 -0400

    comit

--- vadd.c
+++ vadd.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
+#include <omp.h>
 #define N 100000
-#define TOL 0.0000001
+#define TOL  0.0000001
 //
 //  This is a simple program to add two vectors
 //  and verify the results.
@@ -11,34 +12,30 @@ int main()
 {
 
     float a[N], b[N], c[N], res[N];
-    int err = 0;
-    double start_time = omp_get_wtime();
+    int err=0;
 
-    // fill the arrays
-    for (int i = 0; i < N; i++)
-    {
-        a[i] = (float)i;
-        b[i] = 2.0 * (float)i;
-        c[i] = 0.0;
-        res[i] = i + 2 * i;
-    }
+   // fill the arrays
+   #pragma omp parallel for
+   for (int i=0; i<N; i++){
+      a[i] = (float)i;
+      b[i] = 2.0*(float)i;
+      c[i] = 0.0;
+      res[i] = i + 2*i;
+   }
 
-    // add two vectors
-    for (int i = 0; i < N; i++)
-    {
-        c[i] = a[i] + b[i];
-    }
+   // add two vectors
+   #pragma omp target
+   for (int i=0; i<N; i++){
+      c[i] = a[i] + b[i];
+   }
 
-    // test results
-    for (int i = 0; i < N; i++)
-    {
-        float val = c[i] - res[i];
-        val = val * val;
-        if (val > TOL)
-            err++;
-    }
-    double run_time = omp_get_wtime() - start_time;
-
-    printf("time: %ld vectors added with %d errors\n", run_time, err);
-    return 0;
+   // test results
+   #pragma omp parallel for reduction(+:err)
+   for(int i=0;i<N;i++){
+      float val = c[i] - res[i];
+      val = val*val;
+      if(val>TOL) err++;
+   }
+   printf(" vectors added with %d errors\n",err);
+   return 0;
 }
